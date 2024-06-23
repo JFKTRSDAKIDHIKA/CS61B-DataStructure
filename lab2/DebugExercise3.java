b3549634059447deb510fa20c0f943f4f12b6097
JFKTRSDAKIDHIKA <151119886+JFKTRSDAKIDHIKA@users.noreply.github.com>
2024-06-23 15:59:20 +0800

    Add files via upload

---
+++ lab2/DebugExercise3.java
@@ -0,0 +1,32 @@
+/**
+ * Created by jug on 1/22/18.
+ */
+
+public class DebugExercise3 {
+    public static  long countTurnips(In in) {
+        long  totalTurnips = 0;
+        while (!in.isEmpty()) {
+            String vendor = in.readString();
+            String foodType = in.readString();
+            double cost = in.readDouble();
+            long numAvailable = in.readInt();
+            if (foodType.equals("turnip")) {
+                if(numAvailable>0) {
+                    long newTotal = totalTurnips + numAvailable;
+                    totalTurnips = newTotal;
+                }
+                else{
+                    long newTotal = totalTurnips - numAvailable;
+                    totalTurnips = newTotal;
+                }
+            }
+            in.readLine();
+        }
+        return totalTurnips;
+    }
+
+    public static void main(String[] args) {
+        In in = new In("foods.csv");
+        System.out.println(countTurnips(in));
+    }
+}
